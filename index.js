996a3d09b90ad8e2d356a2a6f0acc91e154c62e7
Sarah Alafif <sar1997yah@gmail.com>
2019-11-12 22:22:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Your code here
-let dodger = document.getElementById("dodger");
-function moveDodgerLeft(){
-  document.dodger.style.left;
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
 }
